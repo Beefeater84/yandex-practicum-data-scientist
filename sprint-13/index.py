3e6a8ad6d07941f45594d2f11ee26905fbe04364
Miaseodov Tony <myasoedov.blog@gmail.com>
2026-03-19 10:17:43 +0200

    Refactor code structure for improved readability and maintainability

--- sprint-13/index.py
+++ sprint-13/index.py
@@ -1,3 +1,15 @@
-import sklearn
+from sklearn.datasets import make_regression
 
-print(sklearn.__version__)
+# Генерация данных:
+# 200 примеров (наблюдений)
+# 5 признаков
+# шум с дисперсией 15
+# фиксированное случайное зерно для воспроизводимости
+
+X, y = make_regression(n_samples=200, 
+                       n_features=5,
+                       noise=15,
+                       random_state=42)
+
+# Вывод размеров массивов
+print("Размеры X:", X.shape)
